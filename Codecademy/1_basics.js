ca2f0b83456a833dcf98c3e850a3f9a0cbda7ed5
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2018-12-08 12:39:55 +0100

    JavaScript ex.

--- Codecademy/1_basics.js
+++ Codecademy/1_basics.js
@@ -97,3 +97,93 @@ let newVariable = 'Playing around with typeof.';
 newVariable = 1;
     console.log(typeof 1);
     console.log(typeof newVariable);
+
+
+/****************************
+    CONDITIONAL STATEMENTS
+****************************/
+
+let sale = true;
+sale = false;
+    if (sale) {
+        console.log('Time to buy!');
+    }
+    else {
+        console.log('Wait for a sale.');
+    }
+
+let tempDegrees = 15;
+    if (tempDegrees < 15) {
+        console.log('Stay in home.');
+    }
+    else {
+        console.log('Go outside!');
+    }
+
+let stopLight = 'green'
+let pedestrians = 2;
+    if (stopLight === 'green' && pedestrians === 0) {
+        console.log('Go!');
+    }
+    else {
+        console.log('Stop');
+    }
+    //  && : the and operator
+
+let day = 'saturday';
+    if (day === 'saturday' || day === 'sunday') {
+        console.log('Weekend');
+    }
+    else {
+        console.log('Go to work.');
+    }
+    // || : the or operator
+
+let isHoliday = false;
+    isHoliday ? console.log('Go on holiday!') : console.log('Keep working.');
+
+let isCorrect = true;
+    isCorrect ? console.log('Well done!') : ('Try again!');
+
+let season = 'summer';
+    if (season === 'spring') {
+        console.log('It\'s spring! The trees are budding!');
+    } 
+    else if (season === 'winter') {
+        console.log('It\'s winter! Everything is covered in snow.')
+    }
+    else if (season === 'fall'){
+        console.log('It\'s fall! Leaves are falling!');
+    }
+    else if (season === 'summer'){
+        console.log('It\'s sunny and warm because it\'s summer!');
+    }
+    else {
+        console.log('Invalid season.');
+    }
+
+/* --------------- SWITCH ---------------- */
+
+let finalPosition = 'first place';
+    switch (finalPosition) {
+        case 'first place':
+            console.log('You get the gold medal!');
+            break;
+        case 'second place':
+            console.log('You get the silver medal!');
+            break;
+        case 'third place':
+            console.log('You get the bronze medal!');
+            break;
+        default:
+            console.log('No medal awarded.');
+            break;
+    }
+
+    /*  
+        The 'break' keyword tells the computer to exit the block and not execute any more code or
+        check any other cases inside the code block. Note: Without the break keyword at the end of each 
+        case, the program would execute the code for all matching cases and the default code as well. 
+        This behavior is different from if/else conditional statements which execute only one block of code.
+    */
+
