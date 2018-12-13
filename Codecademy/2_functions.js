8dac3585113f8c5ba5f2d8eb022ab277ec70dfaf
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2018-12-13 11:13:50 +0100

    new functions JS added

--- Codecademy/2_functions.js
+++ Codecademy/2_functions.js
@@ -8,11 +8,16 @@
         /****************************************************/
 
 
+// FUNCTION DECLARATIONS
+
 function greetInSpanish() {
     console.log('Buenas Tardes.');
 }
     greetInSpanish();
 
+
+
+// CALLING A FUNCTION
 
 function sayThanks(name) {
     console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
@@ -20,6 +25,9 @@ function sayThanks(name) {
     sayThanks('Mateusz');
         // Use parameters as plaseholders for information that will be passed to the function when it is called.
     
+
+
+// PARAMETERS AND ARGUMENTS
 
 function greeting (name = 'stranger') {
     console.log(`Hello, ${name}!`)
@@ -29,7 +37,51 @@ function greeting (name = 'stranger') {
         // I used "=" operator to assign the parameter "name" a default value of "stranger".
 
 
+
+// DEFAULT PARAMETERS
+
 function myLanguages (lang1 = 'English', lang2 = 'French', lang3 = 'Spanish', lang4 = 'Polish', lang5 = 'Portuguese') {
     console.log(`I speak ${lang1}, ${lang2}, ${lang3}, ${lang4} and ${lang5}.`)
 }
     myLanguages();
+
+
+
+// RETURN
+
+function rectangleArea(width, height) {
+    let area = width * height;
+}
+    rectangleArea(5, 7) // output: undefinded - The function worked fine, and the computer did calculate the area as 35, but we didn't capture it. We need to return the result.
+
+function rectangleArea2(width,height) {
+    return width * height;
+}
+    console.log(rectangleArea2(5,4)); // output: 20
+
+
+
+// HELPER FUNCTIONS
+
+function multiplyByNineFifths(number){
+    return number * (9/5);
+}
+    function getFahrenheit(celcius) {
+        return multiplyByNineFifths(celcius) + 32;
+    }
+        console.log(getFahrenheit(15)); // output: 59
+
+
+
+// FUNCTION EXPRESSIONS
+
+const plantNeedsWater = function(day) {
+    if(day === 'Wednesday') {
+        return true;
+    } else {
+        return false;
+    }
+};
+    plantNeedsWater('Tuesday');
+    console.log(plantNeedsWater('Tuesday'));
+    // Unlike function declarations, function expressions are not hoisted so they cannot be called before they are defined.
