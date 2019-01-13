804ea2a6f97eb1268232fb785fced74a2d077c0d
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-01-13 09:57:38 +0100

    Data structures ex. added

--- ex: iSA - data structures/modify_array.js
+++ ex: iSA - data structures/modify_array.js
@@ -1,11 +1,39 @@
 var names = ['Janek', 'Bolek', 'Rysiu'];
 
 // 1. TODO. Do tablicy names dodaj parę imion na koniec tablicy
+
+    names.push('Seba', 'Janusz');
+    console.log(names);
+
 // 2. TODO. Z tablicy names usuń imię z końca tablicy
+
+    names.pop();
+    console.log(names);
+
 // 3. TODO. Z tablicy names usuń imię z początku tablicy
+
+    names.shift();
+    console.log(names);
+
 // 4. TODO. Do tablicy names dodaj parę imion na początek tablicy
 
+    names.unshift('Mateusz', 'Borys');
+    console.log(names);
+
 // 5. TODO: Do tablicy names, dołącz tablicę englishNames (zmodyfikuj)
-var englishNames = ['Monica', 'Jessica', 'John'];
+
+    var englishNames = ['Monica', 'Jessica', 'John'];
+    var names = names.concat(englishNames);
+    console.log(names);
 
 // 6. TODO: Skopiuj tablicę names bez referencji
+
+    var copiedNames = [...names];
+    console.log(copiedNames);
+
+
+// .shift(): wyciaga pierwszy element
+// .unshift(): dodaje pierwszy element
+
+// .pop(): usuwa element z konca tablicy
+// .push(el): dodaje na koniec tablicy
