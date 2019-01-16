0edfa339e23acbfddfaa7abfd200b1e8343f20a6
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-01-16 20:38:52 +0100

    Fuel app initial commit

---
+++ Fuel consumption/script.js
@@ -0,0 +1,13 @@
+
+function calculateConsumption(){
+    var distance = document.getElementById('distance').value;
+        // distance in kilometers
+    var fuelConsum = document.getElementById('fuelConsumption').value;
+        // average fuel consumption
+    var costPerL = document.getElementById('costper').value;
+        // cost per 1l
+    var consumed =  fuelConsum / 100 * distance;
+        // fuel needed
+    document.getElementById('consumption').innerHTML = 'Costs of fuel: ' + (consumed*costPerL);
+}
+calculateConsumption();
