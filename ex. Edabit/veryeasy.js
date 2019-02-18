492736650ce45e048295b50af1016a42dd66ed14
Mateusz Iwańczuk <43649657+mateusziwanczuk@users.noreply.github.com>
2019-02-18 18:50:56 +0100

    .shift() .pop() ex added

--- ex. Edabit/veryeasy.js
+++ ex. Edabit/veryeasy.js
@@ -280,5 +280,11 @@ function reverse(bool) {
     }
 
 
+// Create a function that takes an array of numbers and return the first and last elements as a new array.
+
+    const firstLast = arr => [arr.shift(), arr.pop()];
+
+
+
 
 
